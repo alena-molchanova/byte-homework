ab7aa03c668983c3b8a57b5d8cfcc177ff061163
= <=>
2021-09-03 12:59:38 +0300

    hw2

---
+++ hw2/script.js
@@ -0,0 +1,12 @@
+let userAge = 58;
+let userFirstName = "Johnny";
+let userLastName = "Depp";
+
+
+const newUser = { FirstName: userFirstName, LastName: userLastName };
+
+let shoppingList;
+let isOnline = false;
+let salary = 0;
+let cursorCoordinates;
+
